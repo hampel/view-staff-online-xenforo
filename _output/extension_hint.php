d44ad622516e37a1dd18116ade4af3e0e0e38df8
Simon Hampel <simon@hampelgroup.com>
2024-12-30 11:35:21 +1100

    bump version to 1.0.2

--- _output/extension_hint.php
+++ _output/extension_hint.php
@@ -3,6 +3,10 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
+/**
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
 namespace Hampel\ViewStaffOnline\XF\Entity
 {
 	class XFCP_User extends \XF\Entity\User {}
